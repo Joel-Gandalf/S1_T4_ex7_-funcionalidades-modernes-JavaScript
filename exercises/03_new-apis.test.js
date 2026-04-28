11eb6e0eb698d90552f9e5f3c136520aee6c43b8
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-04-28 11:11:57 +0200

    feat: resolved test exercise 03_new-apis-1

--- exercises/03_new-apis.test.js
+++ exercises/03_new-apis.test.js
@@ -1,6 +1,7 @@
 test('03_new-apis-1: should be easier to determine whether a string includes another', () => {
   const sentence = 'It was the best of times. It was the worst of times'
   // crea una variable anomenada `result` que es defineixi com una crida a sentence.includes
+  let result = sentence.includes('It');
   expect(result).toBe(true)
 })
 
