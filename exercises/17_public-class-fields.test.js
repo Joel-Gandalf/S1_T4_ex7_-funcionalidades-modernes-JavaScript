e1096d59577de8195859d9c49b37394409fc694c
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-05-01 22:02:48 +0200

    feat: solved test exercise 17_public-class-fields-1

--- exercises/17_public-class-fields.test.js
+++ exercises/17_public-class-fields.test.js
@@ -1,3 +1,5 @@
+import {jest} from '@jest/globals' 
+
 test('17_public-class-fields-1: public class fields help us avoid .bind-ing everything', () => {
   class FakeReactComponent {
     constructor(props) {
@@ -10,10 +12,13 @@ test('17_public-class-fields-1: public class fields help us avoid .bind-ing ever
     constructor(...args) {
       super(...args)
       // no volem haver de fer això...
-      this.handleClick = this.handleClick.bind(this) // trist :-(
+      // this.handleClick = this.handleClick.bind(this) // trist :-(
     }
     // converteix això en un camp públic de classe perquè s'autolligui
-    handleClick({target: {value}}) {
+    // handleClick({target: {value}}) {
+    //   this.props.onClick(value)
+    // }
+    handleClick = ({target: {value}}) => {
       this.props.onClick(value)
     }
     render() {
@@ -32,3 +37,5 @@ test('17_public-class-fields-1: public class fields help us avoid .bind-ing ever
   expect(onClick).toHaveBeenCalledTimes(1)
   expect(onClick).toHaveBeenCalledWith('hello world')
 })
+
+// En este ejercicio me ha costado mucho seguir el flujo de código y entender como actuan ciertos componentes para poder desgranar para que necesitaba crear un campo público cuando en realidad no es un concepto tan complejo.
